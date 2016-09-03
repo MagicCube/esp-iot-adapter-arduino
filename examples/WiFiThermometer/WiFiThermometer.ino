cc2ba95d1d38c96c16e15d6cc3f8ac0fd5488590
Henry <henry1943@163.com>
2016-09-03 13:05:33 +0800

    Update example

--- examples/WiFiThermometer/WiFiThermometer.ino
+++ examples/WiFiThermometer/WiFiThermometer.ino
@@ -4,55 +4,103 @@
  * @author Henry Li <henry1943@163.com>
  *
  * Setup
- * You need to wire ESP8266's TX to Arduino Pin6 and RX to Arduino Pin7
- * Please also wire a temperature sensor LM35 to analog input Pin0.
+ * You need to wire ESP8266's TX to Arduino Pin2 and RX to Arduino Pin3
+ * Please also wire a temperature sensor LM35 to analog input Pin1.
  *
  */
 
+/*
+ * You need to replace the SSID and password by yourself.
+ */
 #include <SoftwareSerial.h>
 #include <ESP8266IoTAdapter.h>
 
+const String WIFI_SSID = "Henry's Living Room 2.4GHz";
+const String WIFI_PASSWORD = "PASSWORD";
+const int LM35_PIN = 1;
+
+bool wifiConnected = false;
+String ip;
+String url;
+
 // Create ESP8266 adapter with a SoftwareSeiral
-SoftwareSerial softSerial(6, 7);
+SoftwareSerial softSerial(2, 3);
 ESP8266IoTAdapter esp(softSerial);
+
+void setupWifiStationMode()
+{
+    Serial.print("Connecting to WiFi AP...");
+
+    /*
+     * You need to replace the SSID and password by yourself.
+     */
+    esp.connectToAP(WIFI_SSID, WIFI_PASSWORD);
+    while (true)
+    {
+        int status = esp.getWifiStatus();
+        if (status == 2)
+        {
+            Serial.println("ERROR:");
+            Serial.println("Wrong password.");
+            break;
+        }
+        else if (status == 3)
+        {
+            Serial.println("ERROR:");
+            Serial.println("SSID \"" + WIFI_SSID + "\" has not been found.");
+            break;
+        }
+        else if (status == 4)
+        {
+            Serial.println("ERROR:");
+            Serial.println("Fail to connect with \"" + WIFI_SSID + "\".");
+            break;
+        }
+        else if (status == 5)
+        {
+            wifiConnected = true;
+            Serial.println();
+            ip = esp.getIP();
+            url = "http://" + ip + "/data";
+            Serial.println("IP Address: " + ip);
+            break;
+        }
+        else
+        {
+            delay(1000);
+            Serial.print(".");
+        }
+    }
+
+    if (wifiConnected)
+    {
+        Serial.print("Restarting server...");
+        esp.restartServer();
+        Serial.println("done");
+
+        Serial.println();
+        Serial.println("Please open your browser and visit");
+        Serial.println(url);
+        Serial.println();
+    }
+}
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.print("Connecting to WiFi AP...");
-    // Connect ESP8266 to an existing WiFi AP.
-    esp.connectToAP("WiFi SSID", "password");
-    /*
-    * Or you can use configAP() to run in Soft AP mode:
-    * esp.configAP("Your SSID", "password");
-    */
-
-    /*
-    * You can use the following APIs to get more details:
-    * # String esp.getIP()
-    * - Returns ESP8266's IP address.
-    *
-    * # int esp.getWifiStatus()
-    * - 0: STA_IDLE,
-    * - 1: STA_CONNECTING,
-    * - 2: STA_WRONGPWD,
-    * - 3: STA_APNOTFOUND,
-    * - 4: STA_FAIL,
-    * - 5: STA_GOTIP.
-    */
-
-    // Start HTTP server on ESP8266
-    esp.startServer();
-    Serial.print("HTTP Server is now started.");
+    setupWifiStationMode();
 }
 
 void loop()
 {
-    // Read temperature value from Analog PIN 0
-    double temperature = analogRead(0) * (5000 / 1024);
-    // Update values on ESP8266 server every 60 seconds
-    esp.setValue("temperature", temperature);
-    Serial.print("Temperature: ");
-    Serial.println(temperature);
-    delay(60000);
+    if (wifiConnected)
+    {
+        // Read temperature value from Analog PIN 0
+        double temperature = analogRead(LM35_PIN) * (5/10.24);
+        // Update values on ESP8266 server every 60 seconds
+        esp.setValue("temperature", temperature);
+        Serial.print("Temperature: ");
+        Serial.println(temperature);
+        delay(10000);
+    }
 }
